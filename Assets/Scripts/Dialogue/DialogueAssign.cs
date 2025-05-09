6ff6ccd874205c7b71c7c288f08694c195becfdb
Lunalavendermoon <lunalavendermoon@gmail.com>
2025-05-08 22:26:28 -0700

    added new ending

--- Assets/Scripts/Dialogue/DialogueAssign.cs
+++ Assets/Scripts/Dialogue/DialogueAssign.cs
@@ -5,10 +5,21 @@ public class DialogueAssign : MonoBehaviour
     public Dialogue dialogueBeforeEnding;
     public Dialogue goodEnding;
     public Dialogue badEnding;
-    public int threshold;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Dialogue badEnding2;
+    public int threshold = 0;
     void Start()
     {
+        if (threshold == 0) {
+            if (GameManager.outcomeValue >= 9){
+                dialogueBeforeEnding.RootNode.nextDialogue = badEnding;
+            }
+            else if (GameManager.outcomeValue <= 1){
+                dialogueBeforeEnding.RootNode.nextDialogue = badEnding2;
+            }
+            else {
+                dialogueBeforeEnding.RootNode.nextDialogue = goodEnding;
+            }
+        }
         if (GameManager.peopleFed >= threshold) {
             dialogueBeforeEnding.RootNode.nextDialogue = goodEnding;
         }
